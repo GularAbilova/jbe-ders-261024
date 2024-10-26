d3495a5e75969092eedab123e05981da57a8a110
GularAbilova <gular.abilova2000@gmail.com>
2024-10-26 19:13:59 +0400

    first commit

---
+++ src/main/java/az/edu/turing/Main.java
@@ -0,0 +1,43 @@
+package az.edu.turing;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte a = 111;
+        short b = 222;
+        int c = 333;
+        long d = 444L;
+        float e = 555.555f;
+        double f = 666.666;
+        char j = 'A';
+
+        int x = a + b;
+        System.out.println(x);
+
+
+        int number1 = 15;
+        int number2 = 7;
+
+        int number3 = number1 + number2;
+        int number4 = number1 - number2;
+        int number5 = number1 * number2;
+        int number6 = number1 / number2;
+        int number7 = number1 % number2;
+        int number8 = number1 ;
+
+
+
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+        System.out.println(number6);
+        System.out.println(number7);
+
+
+        int a2 = 15;
+        int b3 = 7;
+        int c1 = a2 + b3;
+        System.out.println(c1);
+
+    }
+}
---
+++ src/main/java/az/edu/turing/lesson04/NumberApp.java
@@ -0,0 +1,15 @@
+package az.edu.turing.lesson04;
+
+import java.util.Scanner;
+
+public class NumberApp {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number: \t");
+        int number = scan.nextInt();
+        System.out.println ("Number is:" +number);
+
+
+    }
+}
